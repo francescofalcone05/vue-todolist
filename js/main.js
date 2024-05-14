20455856d62a14210a242b2891aa2f5cb164ba62
francescofalcone05 <ciccio_97@live.it>
2024-05-14 19:15:13 +0300

    bonus1

--- js/main.js
+++ js/main.js
@@ -7,24 +7,51 @@ createApp({
       inputValue: '',
       printValue: '',
       selectDone: 0,
+      color: '',
       toDoList: [
-        'Creare un`array di cose da fare',
-        'Creare un <li> per ogni elemento dell`array',
-        'Stamparli in pagina',
-        'Creare un input dove poter aggiungere cose da fare dentro l`array'
+        {
+          text: 'Creare un`array di cose da fare',
+          done: true
+        },
+        {
+          text: 'Creare un <li> per ogni elemento dell`array',
+          done: true
+        },
+        {
+          text: 'Stamparli in pagina',
+          done: true
+        },
+        {
+          text: 'Creare un input dove poter aggiungere cose da fare dentro l`array',
+          done: true
+        }
       ]
     }
   },
   methods: {
     changeValue() {
-      this.printValue = this.inputValue;
-      this.toDoList.push(this.printValue);
+      this.printValue = { text: this.inputValue, done: true },
+        this.toDoList.push(this.printValue);
       this.inputValue = '';
     },
-    cliccaSeleziona(indice) {
+    cliccaSeleziona(indice,) {
       this.selectDone = indice
-      this.toDoList.splice(this.selectDone,1)
+      this.toDoList.splice(indice,1)
     }
+    // cliccaSeleziona(indice, elemento) {
+    //   this.selectDone = indice
+    //   elemento[indice].done = false
+    //},
+    // cliccaSeleziona(indice,) {
+    //   this.selectDone = indice;
+    // },
+    // changeColor(indice){
+    //   if (this.selectDone==indice){
+    //     (this.color == '') ? this.color = 'green' : this.color = ''
+    //   }else{
+    //     this.color = ''
+    //   }
+    // }
 
   },
   mounted() {
@@ -32,10 +59,4 @@ createApp({
 
 }).mount('#app')
 
-    // changeColor() {
-    //   if (this.color == true) {
-    //     return 'green'
-    //   } else {
-    //     return ''
-    //   }
-    // },
+
